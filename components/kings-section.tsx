93da810e67efd8916c57e1dd47cba17ce7a0d580
ismagab <daismove@gmail.com>
2026-03-04 08:53:49 -0600

    update

--- components/kings-section.tsx
+++ components/kings-section.tsx
@@ -8,7 +8,7 @@ const kings = [
   {
     id: 1,
     name: "Esposos Rodríguez",
-    image: "/images/reyes2025-2026.jpg",
+    image: "/images/king1.jpg",
     electedDate: "Septiembre 2025",
     reignUntil: "Septiembre 2026",
     description: "Reconocidos por su carisma y respeto, muy queridos en comunidad.",
