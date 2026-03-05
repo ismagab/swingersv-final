f2555ff8069b4de5c7f7075ea7f6c6a424d00af4
ismagab <daismove@gmail.com>
2026-03-05 10:12:19 -0600

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,9 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    unoptimized: true,
+    formats: ["image/webp", "image/avif"],
+    deviceSizes: [640, 750, 828, 1080, 1200],
+    minimumCacheTTL: 60,
   },
 }
 
